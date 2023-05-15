dd46a469de234211932549769c715cb5eb8e14cb
João Pedro Barros <joao@joaopedrobarros.com.br>
2023-05-15 20:45:49 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 export type BentoInstance = {
-  /** @deprecated use `initialize` instead. This will be removed on June 1, 2023 */
-  identify(bentoSettings?: BentoSettings): Promise<boolean>;
-  initialize(): Promise<boolean>;
+  /**
+   * Whether Bento is initialized.
+   */
   initialized: boolean;
+  /**
+   * Currenly, this is the same as calling `initialize`.
+   *
+   * @deprecated use `initialize` instead. This will be removed on June 1, 2023 */
+  identify(bentoSettings?: BentoSettings): Promise<boolean>;
+  /**
+   * Tell Bento to initialize.
+   *
+   * Please make sure you've set the `window.bentoSettings` object before.
+   *
+   * Tip: If the account/accountUser is the same but their attributes changed, you should call `initialize` once again.
+   */
+  initialize(): Promise<boolean>;
+  /**
+   * Tell Bento to fully reset. Usually called when account/accountUser has switched.
+   *
+   * Bento will immediately clear all existing state (including the `initialized` flag),
+   * and shutdown any rendered components.
+   *
+   * Tip: You can always re-initialize Bento later.
+   */
   reset(): Promise<boolean>;
 };
 
@@ -14,7 +35,11 @@ export type DynamicAttributes = {
 };
 
 export type BentoSettings = {
-  /** Can be found in Org settings > Integrations */
+  /**
+   * Your unique app identifier provided by Bento.
+   *
+   * Tip: Can be found in Org settings > Integrations
+   */
   appId: string;
   /**
    * Accounts represent your customers or organizations.
@@ -60,17 +85,82 @@ export type BentoSettings = {
   } & DynamicAttributes;
 };
 
-type SidebarToggledEvent = CustomEvent<{
-  isOpen: boolean;
+export enum BentoWindowEvents {
+  /** Fired when Bento has been initialized */
+  initialized = "bento-initialized",
+}
+
+export enum BentoDocumentEvents {
+  /** Fired when a Button or CTA is clicked within Bento */
+  buttonClicked = "bento-buttonClicked",
+  /** Fired when no available guide is found */
+  noGuideFound = "bento-noGuideFound",
+  /** Fired when a guide is loaded */
+  onGuideLoad = "bento-onGuideLoad",
+  /** You can fire this event to tell Bento to close the sidebar component */
+  sidebarClose = "bento-sidebarClose",
+  /** You can fire this event to tell Bento to open the sidebar component, when available */
+  sidebarOpen = "bento-sidebarOpen",
+  /** Fired when the Inline component is loaded */
+  onInlineEmbedLoad = "bento-onInlineEmbedLoad",
+  /** Fired when the Sidebar component is loaded */
+  onSidebarEmbedLoad = "bento-onSidebarEmbedLoad",
+  /** Fired when the Modal component is loaded */
+  onModalEmbedLoad = "bento-onModalEmbedLoad",
+  /** Fired when the Banner component is loaded */
+  onBannerEmbedLoad = "bento-onBannerEmbedLoad",
+  /** Fired when the Tooltip component is loaded */
+  onTooltipEmbedLoad = "bento-onTooltipEmbedLoad",
+}
+
+export type InitializedEvent = CustomEvent<undefined>;
+export type ButtonClickedEvent = CustomEvent<{
+  /** Either the event message or name */
+  message: string;
 }>;
+export type NoGuideFoundEvent = CustomEvent<undefined>;
+export type OnGuideLoadEvent = CustomEvent<{
+  /** Guide base entity id (unique per account) or `undefined` when in preview mode */
+  guideBaseId?: string;
+  /** Whether this guide is of onboarding type */
+  isOnboarding: boolean;
+  /** Whether this guide has been completed */
+  isComplete: boolean;
+  /** List of all Step names */
+  allSteps: Record<string, any>;
+  /** List of all Step names that has been completed */
+  completedSteps: string[];
+  /** List of all Step names that has been skipped */
+  skippedSteps: string[];
+  /** List of all Step names that has been viewed */
+  viewedSteps: string[];
+}>;
+export type SidebarOpenEvent = CustomEvent<undefined>;
+export type SidebarCloseEvent = CustomEvent<undefined>;
+export type OnFormFactorEmbedLoad = CustomEvent<undefined>;
 
 declare global {
   interface Window {
+    /** Holds the current state and API methods of Bento */
     Bento?: BentoInstance;
+    /** The account/accountUser details you must set to initialize Bento */
     bentoSettings?: BentoSettings;
   }
+
+  interface WindowEventMap {
+    [BentoWindowEvents.initialized]: InitializedEvent;
+  }
+
   interface DocumentEventMap {
-    "bento-sidebarToggled": SidebarToggledEvent;
-    /** @todo add support for other events */
+    [BentoDocumentEvents.buttonClicked]: ButtonClickedEvent;
+    [BentoDocumentEvents.noGuideFound]: NoGuideFoundEvent;
+    [BentoDocumentEvents.onGuideLoad]: OnGuideLoadEvent;
+    [BentoDocumentEvents.sidebarClose]: SidebarCloseEvent;
+    [BentoDocumentEvents.sidebarOpen]: SidebarOpenEvent;
+    [BentoDocumentEvents.onInlineEmbedLoad]: OnFormFactorEmbedLoad;
+    [BentoDocumentEvents.onSidebarEmbedLoad]: OnFormFactorEmbedLoad;
+    [BentoDocumentEvents.onModalEmbedLoad]: OnFormFactorEmbedLoad;
+    [BentoDocumentEvents.onBannerEmbedLoad]: OnFormFactorEmbedLoad;
+    [BentoDocumentEvents.onTooltipEmbedLoad]: OnFormFactorEmbedLoad;
   }
 }
